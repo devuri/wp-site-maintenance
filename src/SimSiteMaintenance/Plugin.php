47ef9e3e0106f2b989ba856d2b5326a29c6ec184
devuri <devuri@users.noreply.github.com>
2024-06-07 01:31:23 -0500

    fix: adds `smm_enabled`

--- src/SimSiteMaintenance/Plugin.php
+++ src/SimSiteMaintenance/Plugin.php
@@ -21,7 +21,6 @@ class Plugin extends AbstractPlugin
         add_action( 'admin_init', [ $this, 'settings_init' ] );
     }
 
-
     public function maintenance_mode(): void
     {
         $is_login_register_page = \in_array( $GLOBALS['pagenow'], [ 'wp-login.php', 'wp-register.php' ], true );
@@ -30,7 +29,9 @@ class Plugin extends AbstractPlugin
             return;
         }
 
-        if ( ! current_user_can( 'edit_themes' ) || ! is_user_logged_in() ) {
+        $enabled = get_option( 'smm_enabled', false );
+
+        if ( $enabled && ( ! current_user_can( 'edit_themes' ) || ! is_user_logged_in() ) ) {
             $message = get_option( 'smm_message', 'We are currently performing maintenance. Please check back later.' );
             $header  = get_option( 'smm_header', 'Under Maintenance' );
 
@@ -50,6 +51,7 @@ class Plugin extends AbstractPlugin
 
     public function settings_init(): void
     {
+        register_setting( 'smm_settings', 'smm_enabled' );
         register_setting( 'smm_settings', 'smm_message' );
         register_setting( 'smm_settings', 'smm_header' );
 
@@ -58,6 +60,14 @@ class Plugin extends AbstractPlugin
             __( 'Simple Maintenance Mode Settings', 'site-maintenance' ),
             [ $this, 'settings_section_callback' ],
             'smm_settings'
+        );
+
+        add_settings_field(
+            'smm_enabled',
+            __( 'Enable Maintenance Mode', 'site-maintenance' ),
+            [ $this, 'enabled_render' ],
+            'smm_settings',
+            'smm_section'
         );
 
         add_settings_field(
@@ -77,38 +87,46 @@ class Plugin extends AbstractPlugin
         );
     }
 
+    public function enabled_render(): void
+    {
+        $enabled = get_option( 'smm_enabled', false );
+        ?>
+	        <input type="checkbox" name="smm_enabled" value="1" <?php checked( $enabled, 1 ); ?>>
+	        <?php
+    }
+
     public function header_render(): void
     {
         $header = get_option( 'smm_header', 'Under Maintenance' );
         ?>
-        <input type="text" name="smm_header" value="<?php echo esc_attr( $header ); ?>" size="50">
-        <?php
+	        <input type="text" name="smm_header" value="<?php echo esc_attr( $header ); ?>" size="50">
+	        <?php
     }
 
     public function message_render(): void
     {
         $message = get_option( 'smm_message', 'We are currently performing maintenance. Please check back later.' );
         ?>
-        <textarea name="smm_message" rows="5" cols="50"><?php echo esc_textarea( $message ); ?></textarea>
-        <?php
+	        <textarea name="smm_message" rows="5" cols="50"><?php echo esc_textarea( $message ); ?></textarea>
+	        <?php
     }
 
     public function settings_section_callback(): void
     {
-        echo __( 'Customize the maintenance message and header.', 'site-maintenance' );
+        echo __( 'Customize the maintenance mode settings.', 'site-maintenance' );
     }
 
     public function options_page(): void
     {
         ?>
-        <form action="options.php" method="post">
-            <h2>Site Maintenance Mode</h2>
-            <?php
-            settings_fields( 'smm_settings' );
-			do_settings_sections( 'smm_settings' );
-			submit_button();
-			?>
-        </form>
-        <?php
+	        <form action="options.php" method="post">
+	            <h2>Simple Maintenance Mode</h2>
+	            <?php
+				settings_fields( 'smm_settings' );
+				do_settings_sections( 'smm_settings' );
+				submit_button();
+				?>
+	        </form>
+	        <?php
     }
 }
