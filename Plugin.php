56c0a0eb81403a725b89265b041a234576c8bdec
uri <devuri@users.noreply.github.com>
2026-04-20 14:04:41 -0500

    Add files via upload

---
+++ Plugin.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file is part of the Site MaintenanceWordPress PLugin.
+ *
+ * (c) Uriel Wilson <hello@urielwilson.com>
+ *
+ * Please see the LICENSE file that was distributed with this source code
+ * for full copyright and license information.
+ */
+namespace SimSiteMaintenance;
+class Plugin
+{
+    public function hooks(): void
+    {
+        add_action( 'init', [ $this, 'maintenance_mode' ] );
+        add_action( 'admin_menu', [ $this, 'add_admin_menu' ] );
+        add_action( 'admin_init', [ $this, 'settings_init' ] );
+    }
+    public function maintenance_mode(): void
+    {
+		$maintenance = new Maintenance();
+		$maintenance->page();
+    }
+    public function add_admin_menu(): void
+    {
+        add_options_page( 'Simple Maintenance Mode', 'Maintenance Mode', 'manage_options', 'simple-maintenance-mode', [ $this, 'options_page' ] );
+    }
+    public function settings_init(): void
+    {
+        register_setting(
+            'simsitemaintenance_settings',
+            'simsitemaintenance_enabled',
+            [
+                'type'              => 'boolean',
+                'sanitize_callback' => 'rest_sanitize_boolean',
+            ]
+        );
+        register_setting(
+            'simsitemaintenance_settings',
+            'simsitemaintenance_message',
+            [
+                'type'              => 'string',
+                'sanitize_callback' => 'sanitize_textarea_field',
+            ]
+        );
+        register_setting(
+            'simsitemaintenance_settings',
+            'simsitemaintenance_header',
+            [
+                'type'              => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+            ]
+        );
+        add_settings_section(
+            'simsitemaintenance_section',
+            __( 'Simple Maintenance Mode Settings', 'sim-site-maintenance' ),
+            [ $this, 'settings_section_callback' ],
+            'simsitemaintenance_settings'
+        );
+        add_settings_field(
+            'simsitemaintenance_enabled',
+            __( 'Enable Maintenance Mode', 'sim-site-maintenance' ),
+            [ $this, 'enabled_render' ],
+            'simsitemaintenance_settings',
+            'simsitemaintenance_section'
+        );
+        add_settings_field(
+            'simsitemaintenance_header',
+            __( 'Maintenance Header', 'sim-site-maintenance' ),
+            [ $this, 'header_render' ],
+            'simsitemaintenance_settings',
+            'simsitemaintenance_section'
+        );
+        add_settings_field(
+            'simsitemaintenance_message',
+            __( 'Maintenance Message', 'sim-site-maintenance' ),
+            [ $this, 'message_render' ],
+            'simsitemaintenance_settings',
+            'simsitemaintenance_section'
+        );
+    }
+    public function enabled_render(): void
+    {
+        $enabled = get_option( 'simsitemaintenance_enabled', false );
+        ?>
+	        <input type="checkbox" name="simsitemaintenance_enabled" value="1" <?php checked( $enabled, 1 ); ?>>
+	        <?php
+    }
+    public function header_render(): void
+    {
+        $header = get_option( 'simsitemaintenance_header', 'Under Maintenance' );
+        ?>
+	        <input type="text" name="simsitemaintenance_header" value="<?php echo esc_attr( $header ); ?>" size="50">
+	        <?php
+    }
+    public function message_render(): void
+    {
+        $message = get_option( 'simsitemaintenance_message', 'We are currently performing maintenance. Please check back later.' );
+        ?>
+	        <textarea name="simsitemaintenance_message" rows="5" cols="50"><?php echo esc_textarea( $message ); ?></textarea>
+	        <?php
+    }
+    public function settings_section_callback(): void
+    {
+        echo esc_html__( 'Customize the maintenance mode settings.', 'sim-site-maintenance' );
+    }
+    public function options_page(): void
+    {
+        ?>
+	        <form action="options.php" method="post">
+	            <h2>Simple Maintenance Mode</h2>
+	            <?php
+                settings_fields( 'simsitemaintenance_settings' );
+				do_settings_sections( 'simsitemaintenance_settings' );
+				submit_button();
+				?>
+	        </form>
+	        <?php
+    }
+}
